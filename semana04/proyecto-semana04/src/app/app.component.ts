e8884f39d84cc1fadbe19db172d302f7a303abe7
Juan  Marquina <juanantoniomarquina@gmail.com>
2024-12-03 21:46:41 -0500

    feat: add buttons

--- semana04/proyecto-semana04/src/app/app.component.ts
+++ semana04/proyecto-semana04/src/app/app.component.ts
@@ -1,13 +1,21 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'proyecto-semana04';
+  public titulo: String = 'Mi Primera Pagina con Angular';
+  public contador: number = 10;
+
+  incrementar():void {
+    //this.contador = this.contador +1;
+    this.contador += 1;
+  }
+
+
 }
