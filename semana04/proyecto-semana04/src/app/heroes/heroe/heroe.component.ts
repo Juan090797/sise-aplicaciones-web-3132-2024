45d885481c3f5c4775885da5acf9339fc06831b3
Juan  Marquina <juanantoniomarquina@gmail.com>
2024-12-04 22:55:58 -0500

    feat: se agrego funcion en listar heroes

--- semana04/proyecto-semana04/src/app/heroes/heroe/heroe.component.ts
+++ semana04/proyecto-semana04/src/app/heroes/heroe/heroe.component.ts
@@ -1,12 +1,44 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-heroes-heroe',
   standalone: true,
-  imports: [],
+  imports: [
+    CommonModule
+  ],
   templateUrl: './heroe.component.html',
   styleUrl: './heroe.component.css'
 })
 export class HeroeComponent {
+
+    // SINTAXIS DE INTERPOLACION {{ }}
+    //binding *ngIf
+    // false, undefined, null, 0
+
+    public nombre: string = 'Superman';
+    public edad: number = 100;
+
+    get capitalizarNombre():string {
+      return this.nombre.toUpperCase();
+    }
+
+    getHeroeDescripcion():string {
+      return `${ this.nombre } - ${ this.edad }`;
+    }
+
+
+    cambiarNombre():void{
+      this.nombre = 'Spiderman';
+    }
+    cambiarEdad():void{
+      this.edad = 50;
+    }
+
+    reset():void {
+      this.edad = 100;
+      this.nombre ='Superman';
+    }
+
 
 }
--- semana04/proyecto-semana04/src/app/heroes/listar/listar.component.ts
+++ semana04/proyecto-semana04/src/app/heroes/listar/listar.component.ts
@@ -1,12 +1,32 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-heroes-listar',
   standalone: true,
-  imports: [],
+  imports: [
+    CommonModule
+  ],
   templateUrl: './listar.component.html',
   styleUrl: './listar.component.css'
 })
 export class ListarComponent {
+
+  //ARRAY DE STRINGS
+  //PUBLIC LISTASTRINGS : STRING[]  =['NOMBRE1', 'NOMBRE2', 'NOMBRE3'];
+
+  //ARRAY DE NUMBERS
+  //PUBLIC LISTANUMBERS : STRING[]  =[1, 2, 3];
+
+
+//CREAR UN ARRAY DE STRING CON HEROES
+
+  public heroes: string[] =
+  ['Spiderman', 'Iroman', 'Hulk', 'Superman',
+    'Batman', 'Capitan America'];
+
+    borrarUltimo():void{
+      this.heroes.pop()
+    }
 
 }
