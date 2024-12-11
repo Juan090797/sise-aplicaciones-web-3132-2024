28d3e3bce6384637e00ecdf949db728ea8199d16
Juan  Marquina <juanantoniomarquina@gmail.com>
2024-12-10 21:52:52 -0500

    feat: update componentes

--- semana04/proyecto-semana04/src/app/dbz/pages/main-page/main-page.component.ts
+++ semana04/proyecto-semana04/src/app/dbz/pages/main-page/main-page.component.ts
@@ -1,9 +1,14 @@
 import { Component } from '@angular/core';
+import { ListarComponent } from '../../components/listar/listar.component';
+import { AgregarPersonajeComponent } from '../../components/agregar-personaje/agregar-personaje.component';
 
 @Component({
   selector: 'app-main-page',
   standalone: true,
-  imports: [],
+  imports: [
+    ListarComponent,
+    AgregarPersonajeComponent
+  ],
   templateUrl: './main-page.component.html',
   styleUrl: './main-page.component.css'
 })
